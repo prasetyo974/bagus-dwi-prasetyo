c59f1066b0b95e43115316fdade20c658a61ea4f
Bagus Dwi Prasetyo <bagussprstyoo13@gmail.com>
2026-01-15 16:36:44 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,24 @@
+<?php
+$a=1;
+echo "<select>";
+while ($a <= 31) {
+    echo "<option>$a</option>";
+    $a++;
+}
+echo "<select>";
+
+$a=1;
+echo "<select>";
+while ($a <= 12) {
+    echo "<option>$a</option>";
+    $a++;
+}
+echo "<select>";
+
+$a=2000;
+echo "<select>";
+while ($a <= 2030) {
+    echo "<option>$a</option>";
+    $a++;
+}
+echo "<select>";
